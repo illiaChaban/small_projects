d2877c3bd1916fd2b24ad42733f8973c59d545bc
Illia Chaban <illia.chaban.8@gmail.com>
2019-09-02 22:12:54 -0400

    experiments iwth loading bus svg, completed pure css bus loading animation

---
+++ busDrawing/main.js
@@ -0,0 +1,39 @@
+const svg = document.querySelector('svg');
+let fillTimeoutId;
+let animateTimeoutId;
+
+function prepare() {
+  // set up stroke drawing
+  svg.querySelectorAll('path, line').forEach( (path,i) => {
+    // let color = path.getAttribute('fill');
+    // path.style.setProperty('--color', color);
+    let length = path.getTotalLength();
+    length = Math.floor(length);
+    path.style.setProperty('--length', length);
+    path.style.setProperty('animation-delay', 50 * i + 'ms');
+  });
+}
+
+function cancelDraw() {
+  svg.classList.remove('animate');
+  svg.classList.remove('fill');
+  clearTimeout( fillTimeoutId );
+  clearTimeout( animateTimeoutId );
+}
+
+function draw() {
+  cancelDraw();
+
+  animateTimoutId = setTimeout( () => {
+    // start stroke drawing
+    svg.classList.add('animate');
+    // stop stroke drawing & fill color
+    // fillTimeoutId = setTimeout( () => svg.classList.add('fill'), 3500);
+
+  }, 100)
+}
+
+prepare();
+draw();
+
+// document.getElementById('again').addEventListener('click', draw);
